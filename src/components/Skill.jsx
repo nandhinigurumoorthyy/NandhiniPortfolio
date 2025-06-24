48a1fba37b3569be7f1287fdc393317f081ccb54
Nandhini <nandhinigurumoorthy2003@gmail.com>
2025-06-24 12:14:40 +0530

    commit

--- src/components/Skill.jsx
+++ src/components/Skill.jsx
@@ -9,35 +9,50 @@ import reactIcon from "../assets/react.svg";
 import tailwindIcon from "../assets/tailwindcss.svg";
 import colabIcon from "../assets/colab.png";
 import vsIcon from "../assets/vs_code.png";
+import apiIcon from "../assets/apiIcon.png";
+import c2cIcon from "../assets/c2cIcon.png";
+import githubIcon from "../assets/githubIcon.png";
+import jsonIcon from "../assets/jsonIcon.png";
+import jwtIcon from "../assets/jwtIcon.png";
+import postmanIcon from "../assets/postmanIcon.svg";
 
 const skillItem = [
-    { imgSrc: htmlIcon, label: "HTML", desc: "Markup language" },
-    { imgSrc: cssIcon, label: "CSS", desc: "User Interface" },
-    { imgSrc: jsIcon, label: "JavaScript", desc: "Interaction" },
-    { imgSrc: nodeIcon, label: "NodeJS", desc: "Web Server" },
-    { imgSrc: expressIcon, label: "ExpressJS", desc: "Node Framework" },
-    { imgSrc: mongoIcon, label: "MongoDB", desc: "Database" },
-    { imgSrc: reactIcon, label: "React", desc: "Library" },
-    { imgSrc: tailwindIcon, label: "TailwindCSS", desc: "Styling Framework" },
-    { imgSrc: vsIcon, label: "VS Code", desc: "Code Editor" },
-    { imgSrc: colabIcon, label: "Google Colab", desc: "Python Notebook" }
+  { imgSrc: htmlIcon, label: "HTML", desc: "Markup language" },
+  { imgSrc: cssIcon, label: "CSS", desc: "User Interface" },
+  { imgSrc: jsIcon, label: "JavaScript", desc: "Interaction" },
+  { imgSrc: nodeIcon, label: "NodeJS", desc: "Web Server" },
+  { imgSrc: expressIcon, label: "ExpressJS", desc: "Node Framework" },
+  { imgSrc: mongoIcon, label: "MongoDB", desc: "Database" },
+  { imgSrc: reactIcon, label: "React", desc: "Library" },
+  { imgSrc: tailwindIcon, label: "TailwindCSS", desc: "Styling Framework" },
+  { imgSrc: vsIcon, label: "VS Code", desc: "Code Editor" },
+  { imgSrc: colabIcon, label: "Google Colab", desc: "Python Notebook" },
+  { imgSrc: githubIcon, label: "GitHub", desc: "Version Control" },
+  { imgSrc: apiIcon, label: "RESTful APIs", desc: "Data Communication" },
+  { imgSrc: postmanIcon, label: "Postman API", desc: "API Testing Tool" },
+  { imgSrc: c2cIcon, label: "C2C", desc: "eLearning Platform" },
+  { imgSrc: jwtIcon, label: "JWT", desc: "Authentication" },
+  { imgSrc: jsonIcon, label: "JSON", desc: "Data Format" },
 ];
 
 const Skill = () => {
-    return (
-        <section className="section mb-4 px-4 mx-auto lg:px-8 xl:max-w-6xl">
-            <h2 className="headline-2 text-3xl lg:text-4xl lg:leading-tight font-semibold max-w-max">
-                Essential Tools I Use
-            </h2>
-            <p className="mt-3 mb-8 md:max-w-[70ch] lg:max-w-full text-2xl">Unlocking creativity and efficiency with powerful technologies to build seamless, high-performance web applications.</p>
+  return (
+    <section className="section mb-4 px-4 mx-auto lg:px-8 xl:max-w-6xl">
+      <h2 className="headline-2 text-3xl lg:text-4xl lg:leading-tight font-semibold max-w-max">
+        Essential Tools I Use
+      </h2>
+      <p className="mt-3 mb-8 md:max-w-[70ch] lg:max-w-full text-2xl">
+        Unlocking creativity and efficiency with powerful technologies to build
+        seamless, high-performance web applications.
+      </p>
 
-            <div className="grid gap-3 grid-cols-[repeat(auto-fill,_minmax(250px,_1fr))]">
-                {skillItem.map(({ imgSrc, label, desc }, index) => (
-                    <SkillCard imgSrc={imgSrc} label={label} desc={desc} key={index} />
-                ))}
-            </div>
-        </section>
-    );
+      <div className="grid gap-3 grid-cols-[repeat(auto-fill,_minmax(250px,_1fr))]">
+        {skillItem.map(({ imgSrc, label, desc }, index) => (
+          <SkillCard imgSrc={imgSrc} label={label} desc={desc} key={index} />
+        ))}
+      </div>
+    </section>
+  );
 };
 
 export default Skill;
--- src/components/SkillCard.jsx
+++ src/components/SkillCard.jsx
@@ -2,12 +2,12 @@ import PropTypes from 'prop-types';
 
 const SkillCard = ({ imgSrc, label, desc }) => {
     return (
-        <div className="flex items-center gap-3 ring-2 ring-inset ring-zinc-50/10 rounded-2xl p-3 hover:bg-zinc-800 transition-colors group">
+        <div className="flex items-center text-center gap-3 ring-2 ring-inset ring-zinc-50/10 rounded-2xl p-3 hover:bg-zinc-800 transition-colors group">
             <figure className="bg-zinc-700/50 rounded-lg overflow-hidden w-12 h-12 p-2 group-hover:bg-zinc-900 transition-colors">
                 <img src={imgSrc} width={32} height={32} alt={label} />
             </figure>
 
-            <div>
+            <div className='text-left'>
                 <h3 className="text-slate-100 font-medium">{label}</h3>
                 <p className="text-zinc-400 text-sm">{desc}</p>
             </div>
